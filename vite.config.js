8acb75a3a7f964c450b4c58d9364c062137d14b5
rivershertz <river16@shertz.com>
2024-02-25 10:28:50 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    cors: false,
-    headers: {
-      'access-control-allow-origin': '*',
-    },
-  },
   plugins: [
     vue(),
     federation({
